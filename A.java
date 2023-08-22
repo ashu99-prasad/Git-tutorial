a86d8957820336a73c89a63498e36a4d39318ab7
Meenakshi265 <104715464+Meenakshi265@users.noreply.github.com>
2023-08-22 12:39:12 +0530

    meenakshi

---
+++ A.java
@@ -0,0 +1,15 @@
+package myjava;
+
+public class A{
+
+  public void show(){
+    System.out.println("hello show ....");
+   }
+
+ public static void main(String args[]){
+
+A a=new A();
+ a.show();
+   }
+
+}
---
+++ B.java
@@ -0,0 +1,18 @@
+package myjava1;
+
+import myjava.*;
+
+
+public class B{
+
+  public void Display(){
+    System.out.println("hello show ....");
+   }
+
+ public static void main(String args[]){
+
+   A a=new A();
+   a.show();
+   }
+
+}
